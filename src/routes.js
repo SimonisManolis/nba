9edc836d79984d1232a613adb6606ca7c4d8d952
SimonisManolis <emmasimo2031@yahoo.gr>
2019-10-29 13:40:40 +0200

    before eject

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 
 //COMPONENTS
 import Home from './components/Home/home';
+import Layout from './hoc/Layout/layout';
 
 class Routes extends Component {
     render(){
         return(
-            <Switch>
-                <Route path="/" exact component={Home}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                </Switch>
+            </Layout>
+           
         )
     }
 }
